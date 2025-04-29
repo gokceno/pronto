47de8484e5de062a4fb65fdadbdfef48ba6e2b81
Ege Turanoğlu <egeturanoglu@192.168.1.102>
2025-04-29 10:58:19 +0300

    feat: minimized audio player

--- apps/web/app/components/sticky-audio-player.jsx
+++ apps/web/app/components/sticky-audio-player.jsx
@@ -7,10 +7,10 @@ import { SpeakerLoudIcon,
 SpeakerModerateIcon, 
 SpeakerQuietIcon, 
 SpeakerOffIcon,
-ChevronLeftIcon,
-ChevronRightIcon,
+ChevronDownIcon,
 DotsVerticalIcon,
 HeartIcon, } from "@radix-ui/react-icons";
+import { ChevronUp } from "lucide-react";
 import ReactPlayer from "react-player/lazy";
 
 const StickyAudioPlayer = () => {
@@ -19,6 +19,7 @@ const StickyAudioPlayer = () => {
   const [isClient, setIsClient] = useState(false);
   const [volume, setVolume] = useState(0.3);
   const [playerStatus, setPlayerStatus] = useState("");
+  const [isMinimized, setIsMinimized] = useState(false);
   
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -26,22 +27,18 @@ const StickyAudioPlayer = () => {
     }
   }, []);
 
-  // Use default song name if none available from the player
   const songName = player.songName || player.name || "Now Playing";
   
-  // Handle volume change
   const handleVolumeChange = (e) => {
     setVolume(parseFloat(e.target.value) / 100);
   };
   
-  // Handle stopping playback
   const handleStop = () => {
     setPlayer({ ...player, isPlaying: false });
   };
 
-  // Handle next/previous station (just stops for now)
-  const handleNavigation = () => {
-    setPlayer({ ...player, isPlaying: false });
+  const toggleMinimize = () => {
+    setIsMinimized(!isMinimized);
   };
 
   const handleContinue = () => {
@@ -56,14 +53,15 @@ const StickyAudioPlayer = () => {
     }
   };
 
-  // Check if player should be visible
   if (!player.stationId) {
     return null;
   }
 
   return (
     <div
-      className={`sticky-audio-player flex justify-between items-center gap-8 bg-[#09336B] p-4 rounded-2xl w-full max-w-[57.5rem] fixed inset-x-0 bottom-4 mx-auto z-40`}
+      className={`sticky-audio-player flex items-center gap-8 bg-[#09336B] 
+        ${isMinimized ? 'h-[3.75rem] w-[37.5rem] bottom-0 rounded-t-2xl items-center px-4 pt-[0.75rem] pb-4 ' : 'rounded-2xl bottom-4 p-4 h-[4.75rem] max-w-[52.0625rem]'}
+        fixed inset-x-0 mx-auto z-40 transition-all duration-300`}
     >
       {isClient && player.url && (
         <div className="absolute w-0 h-0 overflow-hidden">
@@ -78,106 +76,152 @@ const StickyAudioPlayer = () => {
         </div>
       )}
       
-      <div className="gap-6">
-        <div className="flex items-center gap-2">
-          <div className="w-12 h-10 flex items-center">
+      {isMinimized ? (
+        <div className="flex items-center pt-2.5 justify-between ">
+          <div className="flex justify-between items-center gap-4">
             <button 
               className="relative flex gap-0.5 justify-between w-10 h-10 items-end"
               onClick={togglePlayback}
             >
-              <div className={`absolute inset-0 flex gap-0.5 justify-between items-end ${player.isPlaying ? 'opacity-100' : 'opacity-0'} transition-opacity duration-300`}>
-                <span className="w-[0.375rem] h-8 bg-[#e6e953] rounded-t-md origin-bottom animate-[bounce_1.3s_ease_infinite_alternate_-1.2s]" />
-                <span className="w-[0.375rem] h-8 bg-[#e6e953] rounded-t-md origin-bottom animate-[bounce_1.3s_ease_infinite_alternate_-2.2s]" />
-                <span className="w-[0.375rem] h-8 bg-[#e6e953] rounded-t-md origin-bottom animate-[bounce_1.3s_ease_infinite_alternate_-3.7s]" />
-                <span className="w-[0.375rem] h-8 bg-[#e6e953] rounded-t-md origin-bottom animate-[bounce_1.3s_ease_infinite_alternate_-0.8s]" />
+              <div className={`absolute mb-2 ml-1 flex gap-0.5 items-end ${player.isPlaying ? 'opacity-100' : 'opacity-0'} transition-opacity duration-300`}>
+                <span className="w-[0.3rem] h-5 bg-[#e6e953] rounded-t-md origin-bottom animate-[bounce_1.3s_ease_infinite_alternate_-1.2s]" />
+                <span className="w-[0.3rem] h-5 bg-[#e6e953] rounded-t-md origin-bottom animate-[bounce_1.3s_ease_infinite_alternate_-2.2s]" />
+                <span className="w-[0.3rem] h-5 bg-[#e6e953] rounded-t-md origin-bottom animate-[bounce_1.3s_ease_infinite_alternate_-3.7s]" />
+                <span className="w-[0.3rem] h-5 bg-[#e6e953] rounded-t-md origin-bottom animate-[bounce_1.3s_ease_infinite_alternate_-0.8s]" />
               </div>
-              <div className={`absolute inset-0 flex items-center justify-center ${player.isPlaying ? 'opacity-0' : 'opacity-100'} transition-opacity duration-300`}>
-                <img src="/assets/icons/paused-bar.svg" alt="Play" className="w-10 h-10" />
+              <div className={`absolute -ml-2 inset-0 flex items-center justify-center ${player.isPlaying ? 'opacity-0' : 'opacity-100'} transition-opacity duration-300`}>
+                <img src="/assets/icons/paused-bar.svg" alt="Play" className="w-8 h-8" />
               </div>
             </button>
-          </div>
-          <div className="w-[12.25rem] overflow-hidden whitespace-nowrap">
-            <div className="inline-block animate-[marquee_5s_linear_infinite] text-base text-white">
-              {songName}
+            <div className="w-[21rem] -ml-4 overflow-hidden whitespace-nowrap">
+              <div className="inline-block animate-[marquee_5s_linear_infinite] text-base text-white">
+                {songName}
+              </div>
             </div>
+            <div className="flex -ml-2 items-center">
+                {volume === 0 ? (
+                  <SpeakerOffIcon className="text-white w-5 h-5" />
+                ) : volume < 0.35 ? (
+                  <SpeakerQuietIcon className="text-white w-5 h-5" />
+                ) : volume < 0.65 ? (
+                  <SpeakerModerateIcon className="text-white w-5 h-5" />
+                ) : (
+                  <SpeakerLoudIcon className="text-white w-5 h-5" />
+                )}
+                <input
+                  type="range"
+                  defaultValue="30"
+                  onChange={handleVolumeChange}
+                  className="mx-2 w-[6.25rem] h-2 bg-gray-700 rounded-full cursor-pointer accent-white transition-opacity duration-200"
+                />
+              </div>
           </div>
-          <div className="flex items-center ml-4">
-            {volume === 0 ? (
-              <SpeakerOffIcon className="mr-2 text-white w-5 h-5" />
-            ) : volume < 0.35 ? (
-              <SpeakerQuietIcon className="mr-2 text-white w-5 h-5" />
-            ) : volume < 0.65 ? (
-              <SpeakerModerateIcon className="mr-2 text-white w-5 h-5" />
-            ) : (
-              <SpeakerLoudIcon className="mr-2 text-white w-5 h-5" />
-            )}
-            <input
-              type="range"
-              defaultValue="30"
-              onChange={handleVolumeChange}
-              className="mx-2 w-[6.25rem] h-2 bg-gray-700 rounded-full cursor-pointer accent-white transition-opacity duration-200"
-            />
-          </div>
+          
+          <button 
+            className="w-6 h-6 ml-5 flex items-center justify-center bg-[#ffffff29] hover:bg-[#167AFE] rounded-full"
+            onClick={toggleMinimize}
+          >
+            <ChevronUp className="text-white w-4 h-4"/>
+          </button>
         </div>
-      </div>
-
-      <div className="flex items-center">
-        <div className="w-16 h-16 rounded-xl flex items-center">
-          {player.imgSrc ? (
-            <img
-              className="w-full h-full rounded-xl object-contain"
-              src={player.imgSrc}
-            />
-          ) : (
-            <div
-              className={`flex items-center flex-shrink-0 h-11 w-16 bg-gradient-to-tr from-[#5539B2] to-[#D4C7FD] rounded-xl justify-center text-white text-base font-semibold select-none capitalize`}
-            >
-              {formatStationName(player.name || "")}
-            </div>
-          )}
-        </div>
-        <div className="flex items-center w-full justify-between">
-          <div className={`flex flex-col pr-4 pl-3`}>
-            <div className={`text-base font-semibold text-white`}>
-              <Truncate>{player.name || ""}</Truncate>
-            </div>
-            <div className={`text-xs text-[#ffffffcc]`}>
-              {player.clickcount || 0} {t("listeningCount")} • {player.votes || 0} {t("likes")}
+      ) : (
+        <>
+          <div className="gap-6">
+            <div className="flex items-center gap-2">
+              <div className="w-12 h-10 flex items-center">
+                <button 
+                  className="relative flex gap-0.5 justify-between w-10 h-10 items-end"
+                  onClick={togglePlayback}
+                >
+                  <div className={`absolute inset-0 flex gap-0.5 justify-between items-end ${player.isPlaying ? 'opacity-100' : 'opacity-0'} transition-opacity duration-300`}>
+                    <span className="w-[0.375rem] h-8 bg-[#e6e953] rounded-t-md origin-bottom animate-[bounce_1.3s_ease_infinite_alternate_-1.2s]" />
+                    <span className="w-[0.375rem] h-8 bg-[#e6e953] rounded-t-md origin-bottom animate-[bounce_1.3s_ease_infinite_alternate_-2.2s]" />
+                    <span className="w-[0.375rem] h-8 bg-[#e6e953] rounded-t-md origin-bottom animate-[bounce_1.3s_ease_infinite_alternate_-3.7s]" />
+                    <span className="w-[0.375rem] h-8 bg-[#e6e953] rounded-t-md origin-bottom animate-[bounce_1.3s_ease_infinite_alternate_-0.8s]" />
+                  </div>
+                  <div className={`absolute inset-0 flex items-center justify-center ${player.isPlaying ? 'opacity-0' : 'opacity-100'} transition-opacity duration-300`}>
+                    <img src="/assets/icons/paused-bar.svg" alt="Play" className="w-10 h-10" />
+                  </div>
+                </button>
+              </div>
+              <div className="w-[12.25rem] overflow-hidden whitespace-nowrap">
+                <div className="inline-block animate-[marquee_5s_linear_infinite] text-base text-white">
+                  {songName}
+                </div>
+              </div>
+              <div className="flex items-center ml-4">
+                {volume === 0 ? (
+                  <SpeakerOffIcon className="mr-2 text-white w-6 h-6" />
+                ) : volume < 0.35 ? (
+                  <SpeakerQuietIcon className="mr-2 text-white w-6 h-6" />
+                ) : volume < 0.65 ? (
+                  <SpeakerModerateIcon className="mr-2 text-white w-6 h-6" />
+                ) : (
+                  <SpeakerLoudIcon className="mr-2 text-white w-6 h-6" />
+                )}
+                <input
+                  type="range"
+                  defaultValue="30"
+                  onChange={handleVolumeChange}
+                  className="mx-2 w-[6.25rem] h-2 bg-gray-700 rounded-full cursor-pointer accent-white transition-opacity duration-200"
+                />
+              </div>
             </div>
           </div>
 
-          <div className={`flex items-center gap-4 `}>
-            <button
-              className={`text-gray-400 hover:text-gray-500 focus:outline-none cursor-pointer group`}
-            >
-              {/* Like button */}
-              <HeartIcon className="text-white w-5 h-5 transition-transform duration-200 ease-in-out group-hover:scale-110 group-hover:text-yellow-300" alt="Like Button" />
-            </button>
+          <div className="flex w-[24.875rem] items-center">
+            <div className="w-16 h-16 rounded-xl flex items-center">
+              {player.imgSrc ? (
+                <img
+                  className="w-full h-full rounded-xl object-contain"
+                  src={player.imgSrc}
+                />
+              ) : (
+                <div
+                  className={`flex items-center flex-shrink-0 h-11 w-16 bg-gradient-to-tr from-[#5539B2] to-[#D4C7FD] rounded-xl justify-center text-white text-base font-semibold select-none capitalize`}
+                >
+                  {formatStationName(player.name || "")}
+                </div>
+              )}
+            </div>
+            <div className="flex items-center w-full justify-between">
+              <div className={`flex flex-col pr-4 pl-3`}>
+                <div className={`text-base font-semibold text-white`}>
+                  <Truncate maxLength={15}>{player.name || ""}</Truncate>
+                </div>
+                <div className={`text-xs text-[#ffffffcc]`}>
+                  {player.clickcount || 0} {t("listeningCount")} • {player.votes || 0} {t("likes")}
+                </div>
+              </div>
 
-            <button
-              className={`text-gray-400 hover:text-gray-500 focus:outline-none group`}
-            >
-              {/* Context_menu button */}
-              <DotsVerticalIcon className="text-white w-5 h-5 transition-transform duration-200 ease-in-out group-hover:scale-110 group-hover:text-yellow-300" alt="Context Menu" />
-            </button>
-          </div>
+              <div className={`flex items-center gap-4 `}>
+                <button
+                  className={`text-gray-400 hover:text-gray-500 focus:outline-none cursor-pointer group`}
+                >
+                  {/* Like button */}
+                  <HeartIcon className="text-white w-5 h-5 transition-transform duration-200 ease-in-out group-hover:scale-110 group-hover:text-yellow-300" alt="Like Button" />
+                </button>
 
-          <div className="flex items-center gap-2 ml-4">
-            <button 
-              className="w-10 h-10 flex items-center justify-center bg-[#ffffff29] hover:bg-[#167AFE] rounded-full"
-              onClick={handleNavigation}
-            >
-              <ChevronLeftIcon className="text-white"/>
-            </button>
-            <button 
-              className="w-10 h-10 flex items-center justify-center bg-[#ffffff29] hover:bg-[#167AFE] rounded-full"
-              onClick={handleStop}
-            >
-              <ChevronRightIcon className="text-white"/>
-              </button>
+                <button
+                  className={`text-gray-400 hover:text-gray-500 focus:outline-none group`}
+                >
+                  {/* Context_menu button */}
+                  <DotsVerticalIcon className="text-white w-5 h-5 transition-transform duration-200 ease-in-out group-hover:scale-110 group-hover:text-yellow-300" alt="Context Menu" />
+                </button>
+              </div>
+
+              <div className="flex items-center gap-2 ml-4">
+                <button 
+                  className="w-10 h-10 flex items-center justify-center bg-[#ffffff29] hover:bg-[#167AFE] rounded-full"
+                  onClick={toggleMinimize}
+                >
+                  <ChevronDownIcon className="text-white"/>
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 };
