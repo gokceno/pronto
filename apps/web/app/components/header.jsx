144c840cc59051db0fa3b319fa5e9a013a971e8c
Ege Turanoğlu <egeturanoglu@192.168.1.106>
2025-05-14 10:35:12 +0300

    refactor: remove homepage tab

--- apps/web/app/components/header.jsx
+++ apps/web/app/components/header.jsx
@@ -94,16 +94,6 @@ export default function Header({ locale, alwaysBlue = false, searchBarStatic = t
             </Link>
           </div>
           <div className="flex items-center -ml-4 md:-ml-0 space-x-2 md:space-x-4">
-            <div className={`flex flex-col items-center group relative ${location.pathname === `/` || location.pathname === `/${locale}` || location.pathname === `/${locale}/` ? 
-              'text-[#E6E953]' : 'text-[#FFFFFF] hover:text-[#E6E953]'}`}>
-              <Link to={generateLocalizedRoute(locale, "/")} className="flex items-center">
-                <HomeIcon className="w-6 h-6 mr-1" />
-                <span className="hidden md:flex font-inter text-base/[1.375rem]">
-                  {t("homePage")}
-                </span>
-              </Link>
-              <div className="absolute bottom-[-8px] w-0 h-[2px] bg-[#E6E953] group-hover:w-full transition-all duration-300 origin-center"></div>
-            </div>
 
             <div className={`flex flex-col items-center group relative ${location.pathname === `/${locale}/genres` ? 'text-[#E6E953]' : 'text-white hover:text-[#E6E953]'}`}>
               <Link
