2b7098e410a1f20387ccc264690791b6f9b24a9f
Ege Turanoğlu <egeturanoglu@192.168.1.102>
2025-05-21 10:57:30 +0300

    feat: no-list menu

--- apps/web/app/routes/deneme.jsx
+++ apps/web/app/routes/deneme.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { RemoveAllFavorites } from '../components/pop-ups/remove-all-favs-menu';
+import { NoListMenu } from '../components/pop-ups/no-list-menu';
 
-export default function DemoRemoveAllFavsMenu() {
+export default function DemoNoListMenu() {
   return (
     <div className="min-h-screen flex items-center justify-center" style={{ background: '#000' }}>
-      <div className="flex flex-col gap-8 items-center">
-        <RemoveAllFavorites type="genre" />
-        <RemoveAllFavorites type="list" />
-        <RemoveAllFavorites type="station" />
-      </div>
+      <NoListMenu />
     </div>
   );
 }
